1d333b048e41aa829ec1257847acd5c6de9eb194
BE3101 <aaron.hein@me.com>
2018-03-27 16:01:07 -0500

    Exception tests added

--- src/test/java/edu/ncsu/csc326/coffeemaker/expections/InventoryExceptionTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/expections/InventoryExceptionTest.java
@@ -1,4 +1,23 @@
 package edu.ncsu.csc326.coffeemaker.expections;
 
+import edu.ncsu.csc326.coffeemaker.Inventory;
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InventoryExceptionTest {
+    @Test
+    public void testInventoryException() {
+        try {
+            Inventory inv = new Inventory();
+            //Add negative inventory. This should trigger an exception.
+            inv.addChocolate("-5");
+            //TODO: Add something here to make a test fail
+        }
+        catch(InventoryException e) {
+            Assert.assertEquals( "Validating Inventory Exception: ",
+                    "Units of chocolate must be a positive integer", e.getMessage());
+        }
+    }
 }
--- src/test/java/edu/ncsu/csc326/coffeemaker/expections/RecipeExceptionTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/expections/RecipeExceptionTest.java
@@ -1,4 +1,20 @@
 package edu.ncsu.csc326.coffeemaker.expections;
 
+import edu.ncsu.csc326.coffeemaker.Recipe;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class RecipeExceptionTest {
+    @Test
+    public void testRecipeException() {
+        try {
+            Recipe r = new Recipe();
+            r.setAmtChocolate("-5");
+        } catch(RecipeException e) {
+            Assert.assertEquals( "Validating Recipe Exception: ",
+                    "Units of chocolate must be a positive integer", e.getMessage());
+        }
+    }
 }
